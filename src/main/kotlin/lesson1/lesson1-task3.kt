f0caf654b8329d2360ac324f8ec026e57b602c16
ffrem <ffrem811@gmail.com>
2024-04-02 19:36:28 +0300

    KS-1-3 create variables val/var, print

---
+++ src/main/kotlin/lesson1/lesson1-task3.kt
@@ -0,0 +1,13 @@
+package lesson1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Юрий Гагарин полетел в космос в $year году \n$hour часов \n$minute минут")
+
+    hour = 10
+    minute = 55
+    print("А приземлился в $hour:$minute ")
+
+}
