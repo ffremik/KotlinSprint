4bf6eae32f4148027a8ad0d6ebb00d479e1286b4
ffrem <ffrem811@gmail.com>
2024-04-03 13:27:50 +0300

    KS-2-1 average

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package lesson2
+
+fun main() {
+    val averageNumber:Double = (3+4+3+5)/4.0
+    println(averageNumber)
+
+    //немного другой подход
+    //val averageNumber = arrayOf(3,4,3,5)
+    //println(averageNumber.average())
+
+}
