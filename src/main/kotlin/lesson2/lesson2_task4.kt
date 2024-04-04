8e1b993d3aae0748eb8529fb2436b1217032b553
ffrem <ffrem811@gmail.com>
2024-04-04 18:36:56 +0300

    KS-2-4 refactor, +const

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,14 +1,15 @@
 package lesson2
 
+const val BONUS = 20.0
+const val PERCENT = 100.0
 fun main() {
-    val crystals = 7
+    val crystals = 7.0
     val iron = 11
 
-    val bonusCrystals = crystals * 1.2
-    val bonusIron = iron * 1.2
+    val bonusCrystals = crystals / PERCENT * BONUS
+    val bonusIron = iron / PERCENT * BONUS
 
-
-    println("Вы добыли $crystals кристалов, +20% бонус = ${bonusCrystals.toInt() - crystals} кристал")
-    println("Вы добыли $iron железо, +20% бонус = ${bonusIron.toInt() - iron} железа")
+    println("Вы добыли $crystals кристалов, +20% бонус = ${bonusCrystals.toInt()} кристал")
+    println("Вы добыли $iron железа, +20% бонус = ${bonusIron.toInt()} железа")
 
 }
