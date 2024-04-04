50d4d7a11455734fe21da6752d454a625ef445e8
ffrem <ffrem811@gmail.com>
2024-04-04 12:11:33 +0300

    KS-3-3 println, ${}, \n

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,18 @@
+package lesson3
+
+fun main() {
+    val number: Int = 5
+
+    println(
+        "Таблица умножения на $number\n" +
+                "$number * 1 = ${number * 1}\n" +
+                "$number * 2 = ${number * 2}\n" +
+                "$number * 3 = ${number * 3}\n" +
+                "$number * 4 = ${number * 4}\n" +
+                "$number * 5 = ${number * 5}\n" +
+                "$number * 6 = ${number * 6}\n" +
+                "$number * 7 = ${number * 7}\n" +
+                "$number * 8 = ${number * 8}\n" +
+                "$number * 9 = ${number * 9}\n"
+    )
+}
