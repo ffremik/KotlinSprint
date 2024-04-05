7ed93bb23e394caabe66ca4d8c7902c1e8ddb038
ffrem <ffrem811@gmail.com>
2024-04-05 17:20:16 +0300

    KS-3-4 исправил формат строки для отправки

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -5,14 +5,14 @@ fun main() {
     var endPosition = "E4"
     var numberMoves = 1
 
-    var message = "Игрок сходил [$startingPosition->$endPosition: Номер хода = $numberMoves]"
+    var message = "[$startingPosition:$endPosition;$numberMoves]"
     println(message)
 
     startingPosition = "D2"
     endPosition = "D3"
     numberMoves = 2
 
-    message = "Игрок сходил [$startingPosition->$endPosition: Номер хода = $numberMoves]"
+    message = "[$startingPosition:$endPosition;$numberMoves]"
     println(message)
 
 }
