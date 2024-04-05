eaa436e6a5dd4c91e922a6f483a994ab2b4836a3
ffrem <ffrem811@gmail.com>
2024-04-05 18:11:33 +0300

    KS-4-4 у меня приходит на ум только через проверку условия, а как без неё реализовать в голову ничего не приходит

---
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -0,0 +1,28 @@
+package lesson4
+
+fun main() {
+    val day = 1
+    var armExerciseCurrent: Boolean
+    var legExerciseCurrent: Boolean
+    var backExerciseCurrent: Boolean
+    var abdominalExerciseCurrent: Boolean
+
+    if (day % 2 == 0) {
+        armExerciseCurrent = false
+        legExerciseCurrent = true
+        backExerciseCurrent = true
+        abdominalExerciseCurrent = false
+    }else {
+        armExerciseCurrent = true
+        legExerciseCurrent = false
+        backExerciseCurrent = false
+        abdominalExerciseCurrent = true
+    }
+
+    println("""
+        Упражнение на руки:   $armExerciseCurrent
+        Упражнения для ног:   $legExerciseCurrent
+        Упражнения для спины: $backExerciseCurrent
+        Упражнения для пресса:$abdominalExerciseCurrent
+    """.trimIndent())
+}
