a764fb60c2af8c6d82f07a1b2fbb527e1d8e280b
ffrem <ffrem811@gmail.com>
2024-04-06 16:25:44 +0300

    KS-5-3 угадывания загаданных чисел

---
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -0,0 +1,25 @@
+package lesson5
+
+fun main() {
+    val correctNumberFirst = 5
+    val correctNumberSecond = 10
+    val userNumberFirst: Int
+    val userNumberSecond: Int
+    var message: String
+
+    println("Необходимо угадать два числа от 0 до 42. Введите числа по очереди:")
+    userNumberFirst = readLine()!!.toInt()
+    userNumberSecond = readLine()!!.toInt()
+
+    if (userNumberFirst == correctNumberFirst || userNumberFirst == correctNumberSecond) {
+        message = "Вы выиграли утешительный приз. Загаданные числа $correctNumberFirst и $correctNumberSecond"
+        if (userNumberSecond == correctNumberFirst || userNumberSecond == correctNumberSecond){
+            message = "Вы выиграли главный приз. Загаданные числа $correctNumberFirst и $correctNumberSecond"
+        }
+    }else{
+        message = "Вы проиграли. Загаданные числа $correctNumberFirst и $correctNumberSecond"
+    }
+
+    println(message)
+
+}
