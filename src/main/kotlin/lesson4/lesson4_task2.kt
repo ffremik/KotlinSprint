f30819c00e505bb65531d4ac49bba20d187ab629
ffrem <ffrem811@gmail.com>
2024-04-06 11:13:06 +0300

    KS-4-2 refactor, постоянные значения поместил в константы, исправил проверку условий

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,22 +1,20 @@
 package lesson4
-
+    const val INITIAL_WEIGHT_AVERAGE = 35
+    const val MAX_WEIGHT_AVERAGE = 100
+    const val MAX_VOLUME = 100
 fun main() {
-    val initialWeightAverage = 35
-    val maxWeightAverage = 100
-    val maxVolume = 100
-
     val weightFirstCargo = 20
     val volumeFirstCargo = 80
     val weightSecondCargo = 50
     val volumeSecondCargo = 100
 
     println("Груз весом $weightFirstCargo кг и объемом $volumeFirstCargo л соответствует категории 'Average': " +
-                "${weightFirstCargo >= initialWeightAverage && 
-                   weightFirstCargo <= maxWeightAverage && 
-                   volumeFirstCargo <= maxVolume}")
+                "${weightFirstCargo >= INITIAL_WEIGHT_AVERAGE && 
+                   weightFirstCargo <= MAX_WEIGHT_AVERAGE && 
+                   volumeFirstCargo < MAX_VOLUME}")
 
     println("Груз весом $weightSecondCargo кг и объемом $volumeSecondCargo л соответствует категории 'Average': " +
-            "${weightSecondCargo >= initialWeightAverage &&
-                    weightSecondCargo <= maxWeightAverage &&
-                    volumeSecondCargo <= maxVolume}")
+                 "${weightSecondCargo >= INITIAL_WEIGHT_AVERAGE &&
+                    weightSecondCargo <= MAX_WEIGHT_AVERAGE &&
+                    volumeSecondCargo < MAX_VOLUME}")
 }
