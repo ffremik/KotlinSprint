ced628559685443bde72241b752c41f09c57d6f2
ffrem <ffrem811@gmail.com>
2024-04-07 12:21:52 +0300

    KS-5-1 пользовательская проверка на бота с помощью if

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -4,13 +4,15 @@ fun main() {
     val num1 = 5
     val num2 = 10
     val userResponse: Int
+    val isRight: Boolean
 
     println("Для входа в приложение необходимо доказать, что вы не бот.\nДля этого необходимо решить пример")
     println("Сколько будет $num1 + $num2?")
+    userResponse = readln().toInt()
 
-    userResponse = readLine()!!.toInt()
+    isRight = userResponse == num1 + num2
 
-    if (userResponse == num1+num2){
+    if (isRight){
         println("Добро пожаловать!")
     }else {
         println("Доступ запрещен!")
