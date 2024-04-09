81b4b503e132af56414f542b6ed0b92e255456ef
ffrem <ffrem811@gmail.com>
2024-04-09 11:00:34 +0300

    KS-5-3 refactor, изменил условия проверки, использования readln(),

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -3,21 +3,18 @@ package lesson5
 fun main() {
     val correctNumberFirst = 5
     val correctNumberSecond = 10
-    val userNumberFirst: Int
-    val userNumberSecond: Int
-    var message: String
+    val message: String
 
     println("Необходимо угадать два числа от 0 до 42. Введите числа по очереди:")
-    userNumberFirst = readLine()!!.toInt()
-    userNumberSecond = readLine()!!.toInt()
+    val userNumberFirst = readln().toInt()
+    val userNumberSecond = readln().toInt()
 
-    if (userNumberFirst == correctNumberFirst || userNumberFirst == correctNumberSecond) {
-        message = "Вы выиграли утешительный приз. Загаданные числа $correctNumberFirst и $correctNumberSecond"
-        if (userNumberSecond == correctNumberFirst || userNumberSecond == correctNumberSecond) {
-            message = "Вы выиграли главный приз. Загаданные числа $correctNumberFirst и $correctNumberSecond"
-        }
+    if ((correctNumberFirst == userNumberFirst || correctNumberFirst == userNumberSecond) && (correctNumberSecond == userNumberFirst || correctNumberSecond == userNumberSecond)) {
+        message = "Вы выиграли главный приз. Загаданные числа были $correctNumberFirst, $correctNumberSecond"
+    } else if ((correctNumberFirst == userNumberFirst || correctNumberFirst == userNumberSecond) || (correctNumberSecond == userNumberFirst || correctNumberSecond == userNumberSecond)) {
+        message = "Вы выиграли утешительный приз. Загаданные числа были $correctNumberFirst, $correctNumberSecond"
     } else {
-        message = "Вы проиграли. Загаданные числа $correctNumberFirst и $correctNumberSecond"
+        message = "Вы проиграли. Загаданные числа были $correctNumberFirst, $correctNumberSecond"
     }
 
     println(message)
